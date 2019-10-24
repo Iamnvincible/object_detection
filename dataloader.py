1d37b1ae32868919c8f9fe2d783f1c37b2f81ca1
Jay Lin <ljlovemichael@hotmail.com>
2019-10-24 15:58:50 +0800

    dataloder

---
+++ dataloader.py
@@ -0,0 +1,6 @@
+
+from carpk import Carpk
+from torch.utils.data import DataLoader
+from torchvision import transforms
+carpklotdataset = Carpk('D:\ISO\datasets\CARPK_devkit', 'train', transform=transforms.Compose(
+    [transforms.Resize(256), transforms.ToTensor()]))

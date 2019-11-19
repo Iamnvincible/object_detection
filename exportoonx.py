ab7dd6c8145e36a8a5305cc99d152508dfae8e69
Jay Lin <ljlovemichael@hotmail.com>
2019-11-19 13:07:07 +0800

    onnx success

--- exportoonx.py
+++ exportoonx.py
@@ -1,11 +1,12 @@
 import torch
 import torchvision
-model_path = r'/media/lin/1TDisk/dataset/UA-DETRAC/model_9.pth'
+model_path = r'd:\dataset\UA-DETRAC\model_9.pth'
 checkpoint = torch.load(
     model_path, map_location='cpu')
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
     num_classes=5, pretrained=False)
 model.load_state_dict(checkpoint['model'])
-x = torch.rand(2, 3, 720, 1280)
-torch.onnx.export(model, args=x, f="carmodel.onnx",
-                  do_constant_folding=True, verbose=True)
+model.eval()
+x = torch.rand(1, 3, 720, 1280)
+torch.onnx.export(model, args=x, f="carmodel2.onnx",
+                  do_constant_folding=True, verbose=True, opset_version=11)
--- orun.py
+++ orun.py
@@ -2,19 +2,43 @@ import onnx
 import onnxruntime
 import numpy as np
 import torch
-onnx_model = onnx.load("carmodel.onnx")
+import torchvision
+from detrac import Detrac
+import dataset.transforms as T
+
+root = r"D:\dataset\UA-DETRAC\Detrac_dataset"
+transforms = []
+transforms.append(T.ToTensor())
+transformscompose = T.Compose(transforms)
+detrac = Detrac(root, imgformat='jpg', transforms=transformscompose)
+img = [detrac[0][0]]
+
+onnx_model = onnx.load("carmodel2.onnx")
 onnx.checker.check_model(onnx_model)
-ort_session = onnxruntime.InferenceSession("carmodel.onnx")
+ort_session = onnxruntime.InferenceSession("carmodel2.onnx")
+
+checkpoint = torch.load(
+    r"D:\dataset\UA-DETRAC\model_9.pth", map_location='cpu')
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+    num_classes=5, pretrained=False)
+model.load_state_dict(checkpoint['model'])
+model.eval()
+torch_out = model(img)
+print(torch_out)
 
 
 def to_numpy(tensor):
     return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
 
-x = torch.randn(1, 1, 224, 224, requires_grad=True)
+x = detrac[0][0].view(1, 3, 720, 1280).numpy()
+
+# print(type(x))
+# x = torch.randn(1, 3, 960, 540, requires_grad=True)
 # compute ONNX Runtime output prediction
-ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+ort_inputs = {ort_session.get_inputs()[0].name: x}
 ort_outs = ort_session.run(None, ort_inputs)
 print(ort_outs)
-
-print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+# np.testing.assert_allclose(
+#     to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+#print("Exported model has been tested with ONNXRuntime, and the result looks good!")
